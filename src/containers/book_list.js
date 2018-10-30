9b2724c99c4189e3f14ef9138dada41af5d9da2f
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-30 13:26:35 +0300

    Create a function renderList

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 
 class BookList extends Component{
+  renderList(){
+  }
   render(){
     return(
       <ul classname="group-list col-sm-4">
-      
+
       </ul>
     );
   }
