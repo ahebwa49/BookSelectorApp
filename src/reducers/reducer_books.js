9b7c75d71a053bdbb3a5029223e9872d58acdcc0
Livingstone Asabahebwa <ahebwa49@gmail.com>
2018-10-30 12:56:02 +0300

    Add a books reducer that will return a list of books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+  return [
+    {title: "Javascript the good parts"},
+    {title: "Things fall apart"},
+    {title: "Burry me in a simple grave"},
+    {title: "Animal farm"},
+    {title: "Romio and Juliet"}
+  ];
+}
